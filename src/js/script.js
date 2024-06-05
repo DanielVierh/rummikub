3bbb123dd6d35048e92484178312111c474e9739
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-06-06 00:13:55 +0200

    additional for loop for create stones

--- src/js/script.js
+++ src/js/script.js
@@ -2,22 +2,24 @@ const colors = ['red', 'orange', 'blue', 'green'];
 const playPieceAmount = 104;
 const jokerAmount = 2;
 let playPieces = [];
+let shuffled_playpieces = [];
 
 
 const matchfield = document.getElementById('matchfield');
+const stack = document.getElementById('stack');
 
 //*ANCHOR -  Init
 window.onload = init();
 function init() {
-   setTimeout(() => {
-    createPlayPieces();
-    createPlayPieces();
-   }, 1000);
+    setTimeout(() => {
+        createPlayPieces();
+    }, 1000);
 }
 
 //*ANCHOR -  PlayPiece
 class PlayPiece {
-    constructor(val, color, isJoker) {
+    constructor(val, color, isJoker, uid) {
+        this.uid = uid;
         this.val = val;
         this.color = color;
         this.isJoker = isJoker;
@@ -26,17 +28,23 @@ class PlayPiece {
 
 //*ANCHOR -  create Play Pieces 
 function createPlayPieces() {
-    for(let c = 0; c < colors.length; c++) {
-        for(let z = 1; z <= 13; z++) {
-            playPieces.push(new PlayPiece(z,colors[c],false))
+    let counter = 1;
+    for (let d = 1; d <= 2; d++) {
+        for (let c = 0; c < colors.length; c++) {
+            for (let z = 1; z <= 13; z++) {
+                playPieces.push(new PlayPiece(z, colors[c], false, `${counter}`));
+                counter++;
+            }
         }
+        playPieces.push(new PlayPiece('🃟', 'black', true, `${counter}`))
     }
 
-    playPieces.push(new PlayPiece('🃟','black',true))
-
- render_Playpiece();
+    shuffled_playpieces = shuffleArray(playPieces);
+    console.log(playPieces);
+    render_Playpiece();
 }
 
+//*ANCHOR -  shuffle Play Pieces 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -48,7 +56,6 @@ function shuffleArray(array) {
 
 //*ANCHOR - Render Playpiece
 function render_Playpiece() {
-    const shuffled_playpieces = shuffleArray(playPieces)
     shuffled_playpieces.forEach((playpiece) => {
 
         let stone = document.createElement('div');
