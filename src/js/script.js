c0b5fbe5fd5cd699fe5fa2414932bb255bb1a8cc
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-06-15 16:11:43 +0200

    add new method for field

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,10 @@ let computerHand = [];
 let playground = [];
 let selectedStone = null;
 let is_firstmove = true;
-let move_Tile_Array = [];
+let is_Move_from_Playerhand = false;
+let move_StonesFromHand_Array = [];
+let is_Move_from_Playground = false;
+let move_StonesFromPlayground_Array = [];
 
 const playgroundElement = document.getElementById('playground');
 const nachziehElement = document.getElementById('nachziehElement');
@@ -19,16 +22,29 @@ const infoLabel = document.getElementById('lbl_info');
 //* -
 //*
 //*### Spielablauf ###
+//* Vor jeder Runde: RenderPlaystones
+//* RenderPlaystones: je nach array, unterschiedliche Event Listener
+//* Wenn Playerhand:
+//* Add EventListener um Stein auf Board abzulegen
 //* 1. Round:
 //* Ich bin am Zug und sehe meine 14 Steine. Ich muss Steine mit einem Mindestwert in Summe von 30 ablegen
 //* Erlaubte Kombos: 1-13 || 3 x gleiche Farbe 
 //* Klicke auf Stein im Board:
+//* 
+//*
+//*
 //*
 //*Klick auf Feld: 
 //*Ermitteln ob Stein vorhanden. Wenn ja, Reihe auswerten und sortieren || Wenn nein:  Stein ablegen und Reihe sortieren/auswerten
 //*
 //*
-//*
+//*Wie werden Veränderungen erfasst
+//* Beispiel man wählt ein Stein vom Playerhand aus und legt diesen auf den Playground:
+//* Klick auf Stein-- 
+//*Stein 
+//* Klick auf Feld
+//* 
+//* 
 
 //*ANCHOR -  Init
 window.onload = init;
@@ -49,7 +65,7 @@ function init() {
     }, 300);
 }
 
- //*ANCHOR Get Object 
+//*ANCHOR Get Object 
 function get_Object_by_ID(uid, place_array) {
     for (let i = 0; i < place_array.length; i++) {
         if (uid === place_array[i].uid) {
@@ -67,6 +83,7 @@ class PlayStones {
         this.isJoker = isJoker;
         this.name = name;
         this.place = '';
+        this.playgroundCoord = '';
     }
 
     change_place(newPlace) {
@@ -101,9 +118,10 @@ function shuffleArray(array) {
     return array;
 }
 
-//*ANCHOR - Render PlayStones
-function renderPlayStones(array, renderSurface) {
+//*ANCHOR - Render PlayStones 
+function renderPlayerhand(array, renderSurface) {
     renderSurface.innerHTML = '';
+
     array.forEach(piece => {
         const tileElement = document.createElement('div');
         tileElement.innerHTML = piece.name;
@@ -112,11 +130,19 @@ function renderPlayStones(array, renderSurface) {
         tileElement.dataset.color = piece.color;
         tileElement.dataset.place = piece.place;
         tileElement.dataset.id = piece.uid;
+
+        //* Event Listener um selectedStone zu übergeben
+        tileElement.addEventListener('click', () => {
+            is_Move_from_Playerhand = true;
+            clearSelection();
+            tileElement.classList.add('selected-stone');
+            selectedStone = piece;
+        })
         renderSurface.appendChild(tileElement);
     });
 }
 
- //*ANCHOR Remove Selection Class
+//*ANCHOR Remove Selection Class
 function clearSelection() {
     document.querySelectorAll('.selected-stone').forEach(element => {
         element.classList.remove('selected-stone');
@@ -131,27 +157,25 @@ function dealInitialStones() {
     }
 }
 
-//* Draw Stones
+//* Aus dem Nachzieh Stape entfernen und in Hand
 function drawTile(hand) {
     if (nachziehstapel.length > 0) {
         hand.push(nachziehstapel.pop());
     }
 }
 
- //*ANCHOR Update
+//*ANCHOR Update
 function initUI() {
     assignPlaces(nachziehstapel, 'nachzieh');
     assignPlaces(computerHand, 'computerHand');
     assignPlaces(playground, 'playground');
     assignPlaces(playerHand, 'playerHand');
 
-    renderPlayStones(playerHand, playerHandElement);
-
-    console.log(playerHand);
+    renderPlayerhand(playerHand, playerHandElement);
 }
 
 
- //*ANCHOR Assign Place
+//*ANCHOR Assign Place
 function assignPlaces(tileArray, placeName) {
     tileArray.forEach(tile => {
         tile.change_place(placeName);
@@ -159,7 +183,7 @@ function assignPlaces(tileArray, placeName) {
 }
 
 
- //*ANCHOR Create Fields
+//*ANCHOR Create Fields
 function createFields() {
     for (let i = 1; i <= 14; i++) {
         const fieldWrapper = document.createElement('div');
@@ -174,27 +198,142 @@ function createFields() {
             field.dataset.holdColor = '';
             field.dataset.holdValue = '';
 
-            field.addEventListener('click', (event) => {
-                //* Check if field hast child
-                if (event.target.hasAttribute('data-hold-id') && event.target.getAttribute('data-hold-id') !== '') {
-                    // Suche nach dem Kindelement, das das Attribut data-id besitzt
-                    let childElement = event.target.querySelector('[data-id]');
-                    if (childElement) {
-                        // Lese das Attribut data-id des Kindelements aus
-                        let dataId = childElement.getAttribute('data-id');
-                        console.log('data-id:', dataId);
-                        // Hier kannst du weiteren Code hinzufügen, um mit dataId zu arbeiten
-                    }
-                }else {
-                    console.log('leer');
-                }
+            // field.addEventListener('click', (event) => {
+            //     //* Check if field hast child
+            //     if (event.target.hasAttribute('data-hold-id') && event.target.getAttribute('data-hold-id') !== '') {
+            //         if (is_Move_from_Playerhand === true) {
+            //             console.warn('Illegal Move');
+            //         } else {
+            //             is_Move_from_Playground = true;
+            //             console.log('is_Move_from_Playground', is_Move_from_Playground);
+            //         }
+            //         // Suche nach dem Kindelement, das das Attribut data-id besitzt
+            //         let childElement = event.target.querySelector('[data-id]');
+            //         if (childElement) {
+            //             // Lese das Attribut data-id des Kindelements aus
+            //             let dataId = childElement.getAttribute('data-id');
+            //             console.log('data-id:', dataId);
+            //             // Hier kannst du weiteren Code hinzufügen, um mit dataId zu arbeiten
+            //         }
+            //     } else {
+            //         //! Check if from Hand or from Playground 
+            //         if (is_Move_from_Playerhand) {
+            //             //* -Selected Stone ist eine Kopie von ausgewählten Stein
+            //             //* -Daher von Playerhand gezogen, muss Stein in Zwischen Array. 
+            //             const board_field_coord = field.id;
+            //             selectedStone.playgroundCoord = board_field_coord;
+            //             console.log('selectedStone', selectedStone);
+            //             move_StonesFromHand_Array.push(selectedStone);
+            //             playground.push(selectedStone);
 
-            });
+            //             const tileElement = document.createElement('div');
+            //             tileElement.innerHTML = selectedStone.name;
+            //             tileElement.className = `${selectedStone.color} stone`;
+            //             tileElement.dataset.joker = selectedStone.isJoker;
+            //             tileElement.dataset.color = selectedStone.color;
+            //             tileElement.dataset.place = selectedStone.place;
+            //             tileElement.dataset.id = selectedStone.uid;
+
+            //             field.appendChild(tileElement);
+
+            //             selectedStone = null;
+
+            //         }
+            //     }
+
+            // });
 
             fieldWrapper.appendChild(field);
         }
 
         playgroundElement.appendChild(fieldWrapper);
+    }
+}
+
+
+
+setTimeout(() => {
+    
+//* Event Listener für Spielfeldklicks hinzufügen
+document.querySelectorAll('.field').forEach(field => {
+    field.addEventListener('click', (event) => {
+        console.log('Field');
+        const targetField = event.currentTarget;
+        const fieldHoldId = targetField.getAttribute('data-hold-id');
+
+        if (selectedStone) {
+            if (is_Move_from_Playerhand) {
+                // Stein vom playerHand zum Spielfeld verschieben
+                moveStoneToField(selectedStone, targetField);
+                is_Move_from_Playerhand = false;
+                selectedStone = null;
+            } else if (fieldHoldId) {
+                console.warn('Illegal Move');
+            }
+        } else if (fieldHoldId) {
+            // Stein vom Spielfeld zurück zur playerHand verschieben
+            const stone = get_Object_by_ID(fieldHoldId, playground);
+            if (stone) {
+                moveStoneToPlayerHand(stone, targetField);
+            }
+        }
+    });
+});
+}, 1000);
+
+function moveStoneToField(stone, field) {
+    // Stein zum Spielfeld-Array hinzufügen
+    playground.push(stone);
+    // Stein von der playerHand entfernen
+    removeFromArray(playerHand, stone);
+    // UI-Update
+    renderPlayerhand(playerHand, playerHandElement);
+    addStoneToField(stone, field);
+}
+
+function moveStoneToPlayerHand(stone, field) {
+    // Stein zur playerHand hinzufügen
+    playerHand.push(stone);
+    // Stein vom Spielfeld-Array entfernen
+    removeFromArray(playground, stone);
+    // UI-Update
+    renderPlayerhand(playerHand, playerHandElement);
+    removeStoneFromField(field);
+}
+
+function removeFromArray(array, stone) {
+    const index = array.findIndex(item => item.uid === stone.uid);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+}
+
+function addStoneToField(stone, field) {
+    // Feld-Attribute aktualisieren
+    field.setAttribute('data-hold-id', stone.uid);
+    field.setAttribute('data-hold-color', stone.color);
+    field.setAttribute('data-hold-value', stone.value);
+
+    // Stein-Element zum Feld hinzufügen
+    const tileElement = document.createElement('div');
+    tileElement.innerHTML = stone.name;
+    tileElement.className = `${stone.color} stone`;
+    tileElement.dataset.joker = stone.isJoker;
+    tileElement.dataset.color = stone.color;
+    tileElement.dataset.id = stone.uid;
+
+    field.appendChild(tileElement);
+}
+
+function removeStoneFromField(field) {
+    // Feld-Attribute zurücksetzen
+    field.removeAttribute('data-hold-id');
+    field.removeAttribute('data-hold-color');
+    field.removeAttribute('data-hold-value');
+    
+    // Stein-Element vom Feld entfernen
+    while (field.firstChild) {
+        field.removeChild(field.firstChild);
     }
 }
 
